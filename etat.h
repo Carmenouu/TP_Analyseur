0e33ee5c4cdda086cc03a0eced5934b9105a76bb
Carmenouu <carmenprevot@hotmail.fr>
2021-02-05 15:38:38 +0100

    Ajout de noms aux états

--- etat.h
+++ etat.h
@@ -6,9 +6,13 @@ using namespace std;
 
 class Etat {
    public:
-      Etat() {  }
-      virtual ~Etat() { }
-      virtual void Transition(Automate automate);
+      Etat();
+      Etat(string nom) : nomEtat(nom) {}
+      virtual ~Etat();
+      virtual bool Transition(Automate automate, string nom);
+   
+   protected:
+      string nomEtat;
 };
 
 /****************************************************
@@ -18,51 +22,51 @@ class Etat {
  * **************************************************/
 class Etat0 : public Etat {
    public:
-      Etat0() : Etat() { }
+      Etat0() : Etat("EO") { }
       ~Etat0() { }
 };
 class Etat1 : public Etat {
    public:
-      Etat1() : Etat() { }
+      Etat1() : Etat("E1") { }
       ~Etat1() { }
 };
 class Etat2 : public Etat {
    public:
-      Etat2() : Etat() { }
+      Etat2() : Etat("E2") { }
       ~Etat2() { }
 };
 class Etat3 : public Etat {
    public:
-      Etat3() : Etat() { }
+      Etat3() : Etat("E3") { }
       ~Etat3() { }
 };
 class Etat4 : public Etat {
    public:
-      Etat4() : Etat() { }
+      Etat4() : Etat("E4") { }
       ~Etat4() { }
 };
 class Etat5 : public Etat {
    public:
-      Etat5() : Etat() { }
+      Etat5() : Etat("E5") { }
       ~Etat5() { }
 };
 class Etat6 : public Etat {
    public:
-      Etat6() : Etat() { }
+      Etat6() : Etat("E6") { }
       ~Etat6() { }
 };
 class Etat7 : public Etat {
    public:
-      Etat7() : Etat() { }
+      Etat7() : Etat("E7") { }
       ~Etat7() { }
 };
 class Etat8 : public Etat {
    public:
-      Etat8() : Etat() { }
+      Etat8() : Etat("E8") { }
       ~Etat8() { }
 };
 class Etat9 : public Etat {
    public:
-      Etat9() : Etat() { }
+      Etat9() : Etat("E9") { }
       ~Etat9() { }
 };
